c8dcb9595ac6f33319582b6c191696218f1585ce
Kazuya Erdos <kazuya_erdos@brown.edu>
2025-01-21 09:27:35 -0500

    try ccls a diff way

--- init.lua
+++ init.lua
@@ -733,6 +733,9 @@ require('lazy').setup({
             },
           },
         },
+        ccls = {
+          lsp = { use_defaults = true },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -787,8 +790,6 @@ require('lazy').setup({
           rust_analyzer = function() end,
         },
       }
-      -- setup ccls
-      require('ccls').setup { lsp = { use_defaults = true } }
     end,
   },
 
