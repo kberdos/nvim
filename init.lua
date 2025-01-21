18ddbcd9585e34dba050c21a4b08bc92da6994ec
Kazuya Erdos <kazuya_erdos@brown.edu>
2025-01-20 19:55:22 -0500

    remove ensure installed

--- init.lua
+++ init.lua
@@ -1026,7 +1026,8 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+      -- ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = {},
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
